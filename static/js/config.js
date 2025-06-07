d731d8fd1ab2843e13b53e5264d104c475857913
joneltolosatablizo <joneltolosatablizo@gmail.com>
2025-06-07 20:08:59 +0800

    PC

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 // Localhost (for development)
-const API_BASE_URL = "http://127.0.0.1:8000/";
+// const API_BASE_URL = "http://127.0.0.1:8000/";
 
 // When deploying, just change the value above to your cloud URL, like:
-// const API_BASE_URL = "https://lms-django-3mep.onrender.com/";
+const API_BASE_URL = "https://sms-v2.onrender.com/";
